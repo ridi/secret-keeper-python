1993674b2c10f05070121beec7e70927dcf43b5c
Lee Chanu <chanu.lee@ridi.com>
2018-10-05 17:04:50 +0900

    Updated version number to test deployment on Travis CI

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=[
         'ridi.secret_keeper',
     ],
-    version='0.1.0',
+    version='0.1.1',
     description='Secret Keeper',
     url='https://github.com/ridi/secret-keeper-python',
     keywords=['secret', 'secret-keeper', 'ridi', 'ridibooks'],
