a1778d4c7428d42ba34a96ea0671b686c8e9c33d
Lee Chanu <chanu.lee@ridi.com>
2018-10-08 10:50:59 +0900

    Modified test to increase coverage

--- test/test_secret_keeper.py
+++ test/test_secret_keeper.py
@@ -1,12 +1,14 @@
+import os
 import unittest
+
+from ridi.secret_keeper import tell, tell_safe, ENVNAME_AWS_ACCESS_KEY, ENVNAME_AWS_SECRET_KEY, ENVNAME_AWS_REGION
+
 try:
     from unittest import mock
 except ImportError:
     import mock
 
 from botocore.exceptions import ClientError
-
-from ridi import secret_keeper
 
 
 class FakeClient(object):
@@ -34,10 +36,14 @@ class FakeClient(object):
 
 class TestSecretKeeperBase(unittest.TestCase):
     def setUp(self):
+        os.environ[ENVNAME_AWS_ACCESS_KEY] = "access_key"
+        os.environ[ENVNAME_AWS_SECRET_KEY] = "secret_key"
+        os.environ[ENVNAME_AWS_REGION] = "us-east-1"
+
         _client = FakeClient({
             "ones": "11111",
         })
-        self._patcher = mock.patch('ridi.secret_keeper._get_client', return_value=_client)
+        self._patcher = mock.patch('boto3.client', return_value=_client)
         self._patcher.start()
 
     def tearDown(self):
@@ -46,16 +52,16 @@ class TestSecretKeeperBase(unittest.TestCase):
 
 class TestSecretKeeperGet(TestSecretKeeperBase):
     def test_existing_keys(self):
-        self.assertEqual(secret_keeper.tell("ones"), "11111")
+        self.assertEqual(tell("ones"), "11111")
 
     def test_non_existing_keys(self):
         with self.assertRaises(ClientError):
-            secret_keeper.tell("twos")
+            tell("twos")
 
 
 class TestSecretKeeperGetSafe(TestSecretKeeperBase):
     def test_existing_keys(self):
-        self.assertEqual(secret_keeper.tell_safe("ones"), "11111")
+        self.assertEqual(tell_safe("ones"), "11111")
 
     def test_non_existing_keys(self):
-        self.assertEqual(secret_keeper.tell_safe("twos"), None)
+        self.assertEqual(tell_safe("twos"), None)
