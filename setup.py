691c5224604ec270e1ca2fb5a122d6fafe367423
Lee Chanu <chanu.lee@ridi.com>
2018-12-05 11:49:31 +0900

    Updated version to 0.3.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=[
         'ridi.secret_keeper',
     ],
-    version='0.3.0.post1',
+    version='0.3.1',
     description='Secret Keeper',
     url='https://github.com/ridi/secret-keeper-python',
     keywords=['secret', 'secret-keeper', 'ridi', 'ridibooks'],
