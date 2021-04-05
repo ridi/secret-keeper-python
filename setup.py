1e687d63ab1ce0a020444bff3562833669c49b0e
Sangbong Lee <sblee@ridi.com>
2021-04-05 13:23:43 +0900

    Release 0.3.2.test1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=[
         'ridi.secret_keeper',
     ],
-    version='0.3.1',
+    version='0.3.2.test1',
     description='Secret Keeper',
     url='https://github.com/ridi/secret-keeper-python',
     keywords=['secret', 'secret-keeper', 'ridi', 'ridibooks'],
@@ -21,6 +21,9 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     install_requires=[
         'boto3>=1.9.16',
